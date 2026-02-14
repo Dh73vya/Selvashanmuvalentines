2809159ec2d6ee3c0da8207c5dce2233cfdb5ad2
dhivy <dhivya@kassavirtanen.fi>
2026-02-14 18:19:40 +0530

    valentine deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Valentinesmy/",   // 👈 ADD THIS LINE
+  base: "/Selvashanmuvalentines/",
   plugins: [react()],
 })
